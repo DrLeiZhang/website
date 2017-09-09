6b19b3a8eecbc19752ee557ea2c02f758355fd71
leizhang@cqu.edu.cn <leizhang@cqu.edu.cn>
2017-09-09 11:51:35 +0800

    Update
    Signed-off-by: leizhang@cqu.edu.cn <leizhang@cqu.edu.cn>

--- img/Image_process.m
+++ img/Image_process.m
@@ -1,5 +1,5 @@
 I=imread('F:\Git\myproject\img\david2.JPG');
-I1=imresize(I(:,:,1),[760 570]);% [570 760] øÌ’’∆¨
+I1=imresize(I(:,:,1),[760 570]);% [570 760] øÌ’’∆¨;  [760 570]’≠’’∆¨
 I2=imresize(I(:,:,2),[760 570]);
 I3=imresize(I(:,:,3),[760 570]);
 image_rgb(:,:,1)=I1;
