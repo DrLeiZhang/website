e899ca00a6888977a6ca198fc8cfd9a2429a21b4
leizhang@cqu.edu.cn <leizhang@cqu.edu.cn>
2017-09-09 11:29:32 +0800

    Update
    Signed-off-by: leizhang@cqu.edu.cn <leizhang@cqu.edu.cn>

--- img/Image_process.m
+++ img/Image_process.m
@@ -1,8 +1,8 @@
-I=imread('F:\Git\myproject\img\LX2.JPG');
+I=imread('F:\Git\myproject\img\HGB3.JPG');
 I1=imresize(I(:,:,1),[570 760]);
 I2=imresize(I(:,:,2),[570 760]);
 I3=imresize(I(:,:,3),[570 760]);
 image_rgb(:,:,1)=I1;
 image_rgb(:,:,2)=I2;
 image_rgb(:,:,3)=I3;
-imwrite(image_rgb,'LX2.jpg','jpg');
+imwrite(image_rgb,'HGB3.jpg','jpg'); % 直接取代原文件夹中的图片
