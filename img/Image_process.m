917c7490d3c45b256c7667c7400ee2c2656240b2
leizhang@cqu.edu.cn <leizhang@cqu.edu.cn>
2017-09-19 12:11:33 +0800

    Update
    Signed-off-by: leizhang@cqu.edu.cn <leizhang@cqu.edu.cn>

--- img/Image_process.m
+++ img/Image_process.m
@@ -1,7 +1,7 @@
 I=imread('F:\Git\myproject\img\ZL4.JPG');
-I1=imresize(I(:,:,1),[570 560]);% [570 760] øÌ’’∆¨;  [760 570]’≠’’∆¨
-I2=imresize(I(:,:,2),[570 560]);
-I3=imresize(I(:,:,3),[570 560]);
+I1=imresize(I(:,:,1),[570 760]);% [570 760] øÌ’’∆¨;  [760 570]’≠’’∆¨
+I2=imresize(I(:,:,2),[570 760]);
+I3=imresize(I(:,:,3),[570 760]);
 image_rgb(:,:,1)=I1;
 image_rgb(:,:,2)=I2;
 image_rgb(:,:,3)=I3;
